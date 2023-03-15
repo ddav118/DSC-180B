ae9a572e9f80709a5eb08b9947b1313b09a76d28
David Davila-Garcia <2018.ddavila@jburroughs.org>
2023-03-14 22:01:57 -0700

    Add files via upload

--- train.py
+++ train.py
@@ -2,57 +2,122 @@ from tqdm import tqdm
 import numpy as np
 import torch
 
+
 # training
-def train1Epoch(epoch_index, model, optimizer, loss_fn, training_loader, writer=None):
+def train1Epoch(epoch_index, model, optimizer, loss_fn, training_loader, model_type):
     model.train()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    losses = np.array([])
+    losses = []
+    if model_type == "VanillaResNet" or model_type == "SegmentedResNet":
+        for i, (image, bnpp, _) in tqdm(
+            enumerate(training_loader), total=len(training_loader)
+        ):
+            image, bnpp = (
+                image.to(device, dtype=torch.float32, non_blocking=True),
+                bnpp.to(device, dtype=torch.float32, non_blocking=True),
+            )
 
-    for i, (image, bnpp, _) in tqdm(
-        enumerate(training_loader), total=len(training_loader)
-    ):
-        image, bnpp = image.to(device, non_blocking=True), bnpp.to(
-            device, non_blocking=True
-        )
+            pred = model(image)
+            loss = loss_fn(torch.squeeze(pred, 1), bnpp)
+            for param in model.parameters():
+                param.grad = None
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
 
-        pred = model(image)
-        loss = loss_fn(torch.squeeze(pred, 1), bnpp)
-        for param in model.parameters():
-            param.grad = None
-        loss.backward()
-        optimizer.step()
-        losses = np.append(losses, loss.item())
+    elif model_type == "MultiChannelResNet":
+        for i, (image1, image2, image3, bnpp, data, _) in tqdm(
+            enumerate(training_loader), total=len(training_loader)
+        ):
+            image1, image2, image3, bnpp, data = (
+                image1.to(device, dtype=torch.float32, non_blocking=True),
+                image2.to(device, dtype=torch.float32, non_blocking=True),
+                image3.to(device, dtype=torch.float32, non_blocking=True),
+                bnpp.to(device, dtype=torch.float32, non_blocking=True),
+                data.to(device, dtype=torch.float32, non_blocking=True),
+            )
+
+            pred = model(image1, image2, image3, data)
+            loss = loss_fn(torch.squeeze(pred, 1), bnpp)
+            for param in model.parameters():
+                param.grad = None
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+    else:
+        for i, (image, bnpp, data, _) in tqdm(
+            enumerate(training_loader), total=len(training_loader)
+        ):
+            image, bnpp, data = (
+                image.to(device, dtype=torch.float32, non_blocking=True),
+                bnpp.to(device, dtype=torch.float32, non_blocking=True),
+                data.to(device, dtype=torch.float32, non_blocking=True),
+            )
+
+            pred = model(image, data)
+            loss = loss_fn(torch.squeeze(pred, 1), bnpp)
+            for param in model.parameters():
+                param.grad = None
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
 
     return np.mean(losses)
 
 
-def test1Epoch(epoch_index, model, loss_fn, valid_loader, tb_writer=None):
+def test1Epoch(epoch_index, model, loss_fn, valid_loader, model_type):
     model.eval()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    losses = np.array([])
+    losses = []
 
     with torch.no_grad():
-        for i, (image, bnpp, _) in tqdm(enumerate(valid_loader), total=len(valid_loader)):
-            image, bnpp = image.to(device, non_blocking=True), bnpp.to(
-                device, non_blocking=True
-            )
-            pred = model(image)
-            loss = loss_fn(torch.squeeze(pred, 1), bnpp).detach()
-            losses = np.append(losses, loss.item())
-            image.detach()
-            bnpp.detach()
-
-    #             out, acts = gcmodel(inpimg)
-    #             loss = nn.CrossEntropyLoss()(out,torch.from_numpy(np.array([600])).to(‘cuda:0’))
-    #             loss.backward()
-    #             grads = gcmodel.get_act_grads().detach().cpu()
-    #             pooled_grads = torch.mean(grads, dim=[0,2,3]).detach().cpu()
-    #             for i in range(acts.shape[1]):
-    #              acts[:,i,:,:] += pooled_grads[i]
-    #             heatmap_j = torch.mean(acts, dim = 1).squeeze()
-    #             heatmap_j_max = heatmap_j.max(axis = 0)[0]
-    #             heatmap_j /= heatmap_j_max
-
+        if model_type == "VanillaResNet" or model_type == "SegmentedResNet":
+            for i, (image, bnpp, _) in tqdm(
+                enumerate(valid_loader), total=len(valid_loader)
+            ):
+                image, bnpp = (
+                    image.to(device, dtype=torch.float32, non_blocking=True),
+                    bnpp.to(device, dtype=torch.float32, non_blocking=True),
+                )
+                pred = model(image)
+                loss = loss_fn(torch.squeeze(pred, 1), bnpp).detach()
+                losses.append(loss.item())
+                image.detach()
+                bnpp.detach()
+        elif model_type == "MultiChannelResNet":
+            for i, (image1, image2, image3, bnpp, data, _) in tqdm(
+                enumerate(valid_loader), total=len(valid_loader)
+            ):
+                image1, image2, image3, bnpp, data = (
+                    image1.to(device, dtype=torch.float32, non_blocking=True),
+                    image2.to(device, dtype=torch.float32, non_blocking=True),
+                    image3.to(device, dtype=torch.float32, non_blocking=True),
+                    bnpp.to(device, dtype=torch.float32, non_blocking=True),
+                    data.to(device, dtype=torch.float32, non_blocking=True),
+                )
+                pred = model(image1, image2, image3, data)
+                loss = loss_fn(torch.squeeze(pred, 1), bnpp).detach()
+                losses.append(loss.item())
+                image1.detach()
+                image2.detach()
+                image3.detach()
+                bnpp.detach()
+                data.detach()
+        else:
+            for i, (image, bnpp, data, _) in tqdm(
+                enumerate(valid_loader), total=len(valid_loader)
+            ):
+                image, bnpp, data = (
+                    image.to(device, dtype=torch.float32, non_blocking=True),
+                    bnpp.to(device, dtype=torch.float32, non_blocking=True),
+                    data.to(device, dtype=torch.float32, non_blocking=True),
+                )
+                pred = model(image, data)
+                loss = loss_fn(torch.squeeze(pred, 1), bnpp).detach()
+                losses.append(loss.item())
+                image.detach()
+                bnpp.detach()
     return np.mean(losses)
